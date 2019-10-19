6f99a04e05d0c7079b2a980eaaca8d92d3383b0b
javicara <javiercaraballo97@gmail.com>
2019-10-19 03:30:43 -0300

    Home mostrado desde BD

--- screens/Home.js
+++ screens/Home.js
@@ -27,12 +27,78 @@ function viajesconductor(email){
   
   }
 
-  viajesconductor('javicara@gmail.com');  
+  const viajes = (setState)=> {
+    API.graphql(graphqlOperation(queries.listGuriviajes)).then(res => {
+      //console.log(res);
+      let array = res.data.listGuriviajes.items
+  
+//      console.log(JSON.stringify(array))
+    const caca=array.map(x => (
+      JSON.parse(x.viajes).map(data =>({
+        email:x.email,
+        nombre:x.name,
+        image:x.image,
+        origen:data.origen,
+        destino:data.destino,
+        fecha:data.fecha,
+        precio:data.precio,
+        celular:2213142511
+        
+      }))
+  
+    ))
+  
+    var merge = [].concat.apply([],caca)
+    
+    //console.log(merge);
+    console.log(articles)
+    setState({viajes:merge});
+     
+    });
+  
+  }
+  //viajesconductor('javicara@gmail.com');  
+
+
+
+
 const { width } = Dimensions.get('screen');
 
 class Home extends React.Component {
+  state= { 
+    viajes:[]
+  } 
+  
+  componentDidMount(){
+    // viajes(this.setState)
+    API.graphql(graphqlOperation(queries.listGuriviajes)).then(res => {
+      //console.log(res);
+      let array = res.data.listGuriviajes.items
+  
+//      console.log(JSON.stringify(array))
+    const caca=array.map(x => (
+      JSON.parse(x.viajes).map(data =>({
+        email:x.email,
+        nombre:x.name,
+        image:x.image,
+        origen:data.origen,
+        destino:data.destino,
+        fecha:data.fecha,
+        precio:data.precio
+        
+      }))
+  
+    ))
+  
+    var merge = [].concat.apply([],caca)
+
+    this.setState({viajes:merge});
+  }
+    )}
+
+
   renderArticles = () => {
-    console.log('caca');
+    
   
     return (
       <ScrollView
@@ -40,7 +106,7 @@ class Home extends React.Component {
         contentContainerStyle={styles.articles}>
          <Block flex>
           <FlatList
-            data={articles}
+            data={this.state.viajes}
             showsVerticalScrollIndicator={false}
             renderItem={({item}) =>
             <Block flex row>
